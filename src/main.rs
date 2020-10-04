6d01fc8b7202d9d2b34ad501ee5fb00ef2cead4e
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2020-10-03 20:22:51 -0400

    Added new scene to demonstrate acceleration

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![allow(unused)]
-use std::fs::{ File, create_dir_all };
-use std::io::prelude::Write;
-use std::path::Path;
-use std::f64::consts;
-use std::sync::Arc;
+use rand::prelude::Rng;
+use std::{
+    f64::consts,
+    fs::{ File, create_dir_all },
+    io::prelude::Write,
+    path::Path,
+    sync::Arc
+};
 
 use raytracer::{
     camera::Camera,
     geom::{
         World,
+        hit::HittableRefs,
         primitives::{ Plane, Sphere },
         objects::{ Prism }
     },
@@ -44,6 +48,10 @@ fn main() {
             eprintln!("Rendering scene 2:");
             render_scene_2();
         },
+        3 => {
+            eprintln!("Rendering scene 3:");
+            render_scene_3();
+        },
         _ => {
             eprintln!("No scene selected.");
             std::process::exit(1);
@@ -56,7 +64,7 @@ fn render_scene_1(render_type: RenderType) {
     let out_width = 640;
     let out_height = 480;
     let fov_deg = 30.0;
-    let aperture = 0.1;
+    let aperture = 0.0;
     let samples = 100;
     let max_depth = 50;
 
@@ -75,8 +83,11 @@ fn render_scene_1(render_type: RenderType) {
     let mat_glass_white = Arc::new(material::Transparent::new(ColorRGB::new(1.0, 1.0, 1.0), 1.52));
     let mat_metal_rough_soft_green = Arc::new(material::Reflective::new(ColorRGB::new(0.6, 0.8, 0.3), 0.3));
 
-    let ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_dif_soft_blue.clone());
-    let red_ball = Sphere::new(Point3::new(0.6, -0.2, -1.0), 0.3, mat_dif_soft_red.clone());
+    let ground = Plane::new(
+        Point3::new(0.0, -0.5, -1.0), 
+        (100.0 * Vec3::I, 100.0 * Vec3::K),
+        mat_dif_soft_blue.clone()
+    );
     let glass_ball = Sphere::new(Point3::new(-0.27, -0.1, -0.8), 0.4, mat_glass_white.clone());
     let green_metal_ball = Sphere::new(Point3::new(0.0, 0.0, -1.5), 0.5, mat_metal_rough_soft_green.clone());
     let gray_plane = Plane::new(
@@ -84,6 +95,7 @@ fn render_scene_1(render_type: RenderType) {
         (0.25 * (Vec3::I - Vec3::K), 0.25 * (Vec3::I + Vec3::J)),
         mat_dif_soft_gray.clone()
     );
+    let red_ball = Sphere::new(Point3::new(0.6, -0.2, -1.0), 0.3, mat_dif_soft_red.clone());
 
     let world = World::new(vec![
         Arc::new(ground),
@@ -172,7 +184,7 @@ fn render_scene_2() {
     let mat_dif_white = Arc::new(material::DiffuseLambert::new(ColorRGB::new(1.0, 1.0, 1.0)));
     let mat_dif_red = Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.57, 0.025, 0.025)));
     let mat_dif_green = Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.025, 0.236, 0.025)));
-    let mat_light = Arc::new(material::Emissive::new(ColorRGB::new(1.0, 0.67, 0.21), 15.0));
+    let mat_light = Arc::new(material::Emissive::new(ColorRGB::new(1.0, 0.67, 0.21), 16.3));
 
     let floor = Plane::new(
         Point3::new(278.0, 0.0, 279.6),
@@ -241,4 +253,71 @@ fn render_scene_2() {
 
     // Uncomment to watch render live
     // raytracer::write_ppm(&world, &camera, "cornell.ppm", &config);
+}
+
+fn render_scene_3() {
+    let out_width = 640;
+    let out_height = 480;
+    let fov_deg = 30.0;
+    let aperture = 0.1;
+    let samples = 100;
+    let max_depth = 50;
+
+    let background = |t| {
+        math::lerp(colors::SKYBLUE, colors::WHITE, t)
+    };
+    let config = raytracer::ImageConfig {
+        width: out_width, height: out_height,
+        samples: samples, max_depth: max_depth,
+        background: Arc::new(background)
+    };
+
+    let mat_dif_soft_blue = Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.3, 0.5, 0.8)));
+    let materials = [
+        Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.8, 0.3, 0.4))), // reddish
+        Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.8, 0.6, 0.3))), // orangish
+        Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.8, 0.8, 0.4))), // yellowish
+        Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.5, 0.8, 0.3))), // greenish
+        Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.3, 0.7, 0.8))), // bluish
+        Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.4, 0.3, 0.8))), // indigoish
+        Arc::new(material::DiffuseLambert::new(ColorRGB::new(0.6, 0.3, 0.8)))  // violetish
+    ];
+
+    let ground = Plane::new(
+        Point3::new(0.0, -0.5, -1.0), 
+        (100.0 * Vec3::I, 100.0 * Vec3::K),
+        mat_dif_soft_blue.clone()
+    );
+
+    let mut red_balls: HittableRefs = vec![Arc::new(ground)];
+    let mut rng = rand::thread_rng();
+    for i in 0..500 {
+        let rand_x: f64 = -(rng.gen::<f64>() * 50.0) + 25.0;
+        let rand_z: f64 = -(rng.gen::<f64>() * 50.0);
+        let material = materials[i % materials.len()].clone();
+        let radius = 0.5 * rng.gen::<f64>() + 0.3;
+        red_balls.push(Arc::new(
+            Sphere::new(Point3::new(rand_x, -0.5 + radius, rand_z), radius, material)
+        ));
+    }
+
+    let world = World::new(red_balls);
+
+
+    let camera =
+        Camera::new(Point3::new(0.7, 2.0, 3.0), &Point3::new(0.0, 0.0, -10.0),
+            fov_deg, aperture, out_width, out_height);
+
+    // Single-threaded
+    // let mut file = File::create(&Path::new("temp.ppm")).unwrap();
+    // file.write_all(raytracer::create_ppm(&world, &camera, &config).as_bytes()).unwrap();
+
+    // Multi-threaded
+    let world_arc = Arc::new(world);
+    let camera_arc = Arc::new(camera);
+    let config_arc = Arc::new(config);
+    raytracer::write_ppm_threaded(world_arc, camera_arc, "balls.ppm", config_arc);
+
+    // Uncomment to watch render live
+    // raytracer::write_ppm(&world, &camera, "temp.ppm", &config);
 }
